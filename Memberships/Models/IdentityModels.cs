f1862395b46bde78d942b5e6b0b57cc7a8392814
Brett Hartman <brehardev@gmail.com>
2019-03-25 11:07:13 -0700

    Create Product, ProductLinkText, and ProductType entities

--- Memberships/Models/IdentityModels.cs
+++ Memberships/Models/IdentityModels.cs
@@ -25,6 +25,9 @@ namespace Memberships.Models
         public DbSet<Part> Parts { get; set; }
         public DbSet<ItemType> ItemTypes { get; set; }
         public DbSet<Item> Items { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductType> ProductTypes { get; set; }
+        public DbSet<ProductLinkText> ProductLinkTexts { get; set; }
 
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)

9ba35d77cd73624ff85bc81266ff423675576e36
Brett Hartman <brehardev@gmail.com>
2019-03-25 12:45:30 -0700

    Add columns to AspNetUsers table

--- Memberships/Models/IdentityModels.cs
+++ Memberships/Models/IdentityModels.cs
@@ -1,4 +1,5 @@
-﻿using System.Data.Entity;
+﻿using System;
+using System.Data.Entity;
 using System.Security.Claims;
 using System.Threading.Tasks;
 using Memberships.Entities;
@@ -10,6 +11,10 @@ namespace Memberships.Models
     // You can add profile data for the user by adding more properties to your ApplicationUser class, please visit https://go.microsoft.com/fwlink/?LinkID=317594 to learn more.
     public class ApplicationUser : IdentityUser
     {
+        public string FirstName { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime Registered { get; set; }
+
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
         {
             // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
